d6944f78cd85f15b10d52b566969ff440cd76123
wpJAVA <wp13093812525>
2017-04-07 14:50:25 +0800

    change

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+
+    public static void test1() {
+        System.out.println("1231321");
+    }
 }
