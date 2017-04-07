5fd0b0e09e9bd2460574a7f8c1f062ab7a4e2939
wpJAVA <wp13093812525>
2017-04-07 14:57:28 +0800

    change2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
 
     public static void test1() {
-        System.out.println("1231321");
+        System.out.println("1231322222221");
+    }
+
+    public static void test2() {
+        System.out.println("123ttttt1321");
     }
 }
